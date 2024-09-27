b20fb0e58c5237caccebf47493a74ab11eb606b0
jlor87 <joshualor87@yahoo.com>
2024-09-27 16:41:37 -0400

    Added development log

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../utilities/db');
+const { check, validationResult } = require('express-validator');
 
-router.post('/', async (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
+router.post('/',
+    
+    // Middleware to sanitize username and password fields from the user
+    check('username')
+        .trim() // Remove spaces
+        .not().isEmpty() // Cannot be empty
+        .escape(), // Prevents injections
+    check('password')
+        .trim()
+        .not().isEmpty() 
+        .escape(),
+
+    async (req, res) => {
+
+    // Validating the user input
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){ // If an error is found, "errors" will not be empty
+        return res.status(400).json({ errors: errors.array() }); // Show the error messages to user
+    }
+
+    // Else, continue onward with logging in the user
+    var { username, password} = req.body;
 
     try{
         var [retrievedDataFromDb, otherFieldsFromDb] = await db.execute(`SELECT * FROM users WHERE username=? AND password=?;`, [username, password])
